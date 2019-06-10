5a7724b7e2ff8b815e118d57a0d1dbc752fd11b9
Anton Samsonov <asamsonov03@yandex.ru>
2019-06-10 19:25:11 +0300

    MoneyService done

--- src/main/java/services/IMoneyService.java
+++ src/main/java/services/IMoneyService.java
@@ -4,8 +4,8 @@ public interface IMoneyService {
 
     void debitAccount(long accountId, int sum);
 
-    void creditAccount(long accountId, int sum);
+    boolean creditAccount(long accountId, int sum);
 
-    void transferMoney(long firstId, long secondId, int sum);
+    boolean transferMoney(long debitId, long creditId, int sum);
 
 }
--- src/main/java/services/MoneyService.java
+++ src/main/java/services/MoneyService.java
@@ -1,20 +1,33 @@
 package services;
 
 import dao.repository.IAccountRepository;
+import dto.entity.Account;
 
 public class MoneyService implements IMoneyService {
 
     private IAccountRepository accountRepository;
 
     public void debitAccount(long accountId, int sum) {
-
+        Account account = accountRepository.getAccountById(accountId);
+        account.setBalance(account.getBalance() + sum);
     }
 
-    public void creditAccount(long accountId, int sum) {
+    public boolean creditAccount(long accountId, int sum) {
+        Account account = accountRepository.getAccountById(accountId);
+        int balance = account.getBalance();
+        if (balance < sum) {
+            return false;
+        }
 
+        account.setBalance(balance - sum);
+        return true;
     }
 
-    public void transferMoney(long firstId, long secondId, int sum) {
-
+    public boolean transferMoney(long debitId, long creditId, int sum) {
+        if (creditAccount(creditId, sum)) {
+            debitAccount(debitId, sum);
+            return true;
+        }
+        return false;
     }
 }
